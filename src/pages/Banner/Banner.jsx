21b4e773e7602dc9a27211df13992cda046b144d
Tarek Aziz <mdaziz3289@gmail.com>
2023-05-04 13:08:39 +0600

    banner section added

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -1,11 +1,36 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import bannerFood from '../../../public/photos/bannerFood.jpg';
+import { Row } from 'react-bootstrap';
 
 const Banner = () => {
     return (
-        <div>
-            <h3>banner</h3>
+      <Row xs={1} md={2} className="g-4 text-center ">
+        <div className="d-flex align-items-center">
+          <div>
+            <h1 className="text-success">Spice Garden</h1>
+            <h6>
+              Transport yourself to an exotic garden of flavor with Spice
+              Garden's authentic Indian cuisine.
+            </h6>
+          </div>
         </div>
+        <img src={bannerFood} alt="" />
+      </Row>
+
+      // <div className=" gap-5 my-5 text-center">
+      //   <div className="d-flex align-items-center  mx-auto ">
+      //     <div>
+      //       <h1 className="text-success">Spice Garden</h1>
+      //       <h6>
+      //         Transport yourself to an exotic garden of flavor with Spice
+      //         Garden's authentic Indian cuisine.
+      //       </h6>
+      //     </div>
+      //   </div>
+      //   <img src={bannerFood} alt="" />
+      // </div>
     );
 };
 
